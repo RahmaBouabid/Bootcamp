b1727bd9c3062f010ab96cd7a9e8c68ecbccce45
RahmaBouabid <rahma.bouabid1991@gmail.com>
2022-02-24 18:38:53 +0100

    premier projet spring

---
+++ src/main/java/com/sip/ams/Amsdata1Application.java
@@ -0,0 +1,13 @@
+package com.sip.ams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Amsdata1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Amsdata1Application.class, args);
+	}
+
+}
